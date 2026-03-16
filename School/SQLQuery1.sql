966a9492f5c4825d4e1a155980b5b8cd7769f3cc
Kartofechka <126353546+Kartofechka@users.noreply.github.com>
2026-03-16 15:44:15 +0300

    Add files via upload

--- School/SQLQuery1.sql
+++ School/SQLQuery1.sql
@@ -28,15 +28,28 @@ PRIMARY KEY(ID),
 )
 
 
-insert into Subjects values ('Proga', '10')
-insert into Teachers values ('ChinGinShan', '10', '1')
-insert into Grades values ('10', '1')
-insert into Students values ('Shurupovert', '1')
+insert into Subjects values ('Matan', '10')
+insert into Teachers values ('Pididi', '10', '2')
+insert into Grades values ('9', '1')
+insert into Grades values ('10', '2')
+insert into Grades values ('2', '1')
+insert into Grades values ('6', '1')
+insert into Grades values ('5', '2')
+insert into Grades values ('7', '2')
+insert into Grades values ('3', '2')
+insert into Students values ('Vladik', '2')
+insert into Students values ('Georje', '3')
+insert into Students values ('Denchik', '4')
+insert into Students values ('Antonina', '5')
+insert into Students values ('Dizel', '6')
+insert into Students values ('Kirpich', '7')
+insert into Students values ('Kiryusha', '8')
 
-SELECT Students.ID, Students.FIO, Grades.Grades, Teachers.FIO, Subjects.Name FROM Students
-	JOIN Grades ON Grades.ID = Students.Grades
-	JOIN Teachers ON Teachers.ID = Grades.Teacher
-	JOIN Subjects ON Subjects.ID = Teachers.Subj;
+SELECT Teachers.FIO as teachers, Students.FIO as students, Students.Grades FROM Teachers
+	JOIN Grades on Teachers.ID = Grades.Teacher
+	JOIN Students on Grades.ID = Students.Grades
+	where Teachers.FIO = 'ChinGinShan'
+
 
 
 
